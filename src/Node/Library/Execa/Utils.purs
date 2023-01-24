91fde43aeff03567adac08d9b2260223aaa8ef8c
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-01-24 07:36:59 -0600

    Add utf8 buffer-string util

--- src/Node/Library/Execa/Utils.purs
+++ src/Node/Library/Execa/Utils.purs
@@ -6,11 +6,20 @@ import Data.Function.Uncurried (Fn2, runFn2)
 import Data.Symbol (class IsSymbol)
 import Effect (Effect)
 import Effect.Exception (Error)
+import Node.Buffer.Immutable (ImmutableBuffer)
+import Node.Buffer.Immutable as ImmutableBuffer
+import Node.Encoding (Encoding(..))
 import Node.Stream (Duplex)
 import Prim.Row as Row
 import Record as Record
 import Type.Proxy (Proxy)
 import Unsafe.Coerce (unsafeCoerce)
+
+utf8 :: { toString :: ImmutableBuffer -> String, toBuffer :: String -> ImmutableBuffer }
+utf8 = { toString, toBuffer }
+  where
+  toString = ImmutableBuffer.toString UTF8
+  toBuffer = flip ImmutableBuffer.fromString UTF8
 
 newtype CustomError :: forall k. k -> Type
 newtype CustomError rows = CustomError Error
