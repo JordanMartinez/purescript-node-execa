74dad9f118926bf8b8d8ec311d1368af959ae1a9
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-01-24 19:42:41 -0600

    Add simple cat tests

--- test/Test/Node/Library/Execa.purs
+++ test/Test/Node/Library/Execa.purs
@@ -29,6 +29,23 @@ spec = do
       case result of
         Right _ -> fail "Expected EPIPE error"
         Left e -> e.message `shouldContain` "EPIPE"
+    describe "`cat` tests" do
+      it "input is file" do
+        spawned <- execa "cat" [ "test.dhall" ] identity
+        result <- joinFiber spawned.run
+        case result of
+          Right r -> r.stdout `shouldContain` "let config ="
+          Left e -> fail e.message
+      it "input is buffer" do
+        spawned <- execa "cat" [ "-" ] identity
+        liftEffect do
+          buf <- Buffer.fromString "test" UTF8
+          void $ Stream.write (stdin spawned.childProcess) buf mempty
+          void $ Stream.end (stdin spawned.childProcess) mempty
+        result <- joinFiber spawned.run
+        case result of
+          Right r -> r.stdout `shouldEqual` "test"
+          Left e -> fail e.message
   describe "execaSync" do
     pure unit
   describe "execaCommand" do
