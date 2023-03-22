b3747add9bef0f2e0a6e56de1647936227007346
JordanMartinez <jordanalex.martinez@protonmail.com>
2023-03-21 20:52:23 -0500

    Drop wrapping quotes; parse any escaped char (#5)

--- test/Test/Node/Library/Execa.purs
+++ test/Test/Node/Library/Execa.purs
@@ -5,7 +5,8 @@ import Prelude
 import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
-import Data.String as String
+import Data.Tuple (fst, snd)
+import Data.Tuple.Nested ((/\))
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Node.Library.Execa (execa, execaCommand, execaCommandSync, execaSync)
@@ -117,26 +118,53 @@ spec = do
   describe "parseCommand" do
     let
       shouldBeFileArgs file args = do
-        let result = parseCommand' $ file <> " " <> Array.intercalate " " args
-        Right (String.trim file) `shouldEqual` (map _.file) result
-        Right (map String.trim args) `shouldEqual` (map _.args) result
+        let result = parseCommand' $ (fst file) <> " " <> (Array.intercalate " " $ map fst args)
+        Right (snd file) `shouldEqual` (map _.file) result
+        Right (map snd args) `shouldEqual` (map _.args) result
 
       escapeSlash = """\"""
+      backslash = escapeSlash
+      space = " "
       escapedSpace = escapeSlash <> " "
       dquote = "\""
       squote = "'"
       escSQuote = escapeSlash <> squote
       escDQuote = escapeSlash <> dquote
+      escBackslash = escapeSlash <> backslash
 
     it "should work despite extra spaces" do
-      shouldBeFileArgs " file  " [ "    arg1", "arg2   ", "   arg3   " ]
+      shouldBeFileArgs
+        (" file  " /\ "file")
+        [ "    arg1" /\ "arg1"
+        , "arg2   " /\ "arg2"
+        , "   arg3   " /\ "arg3"
+        ]
 
-    it "should account for escaped spaces, double-quotes, and single-quotes" do
-      shouldBeFileArgs "file" [ "partA" <> escapedSpace <> escDQuote <> escSQuote <> "partB" ]
+    it "should account for escaped spaces, double-quotes, single-quotes, and back slashes" do
+      shouldBeFileArgs
+        ("file" /\ "file")
+        [ ("a" <> escapedSpace <> "b") /\ ("a" <> space <> "b")
+        , ("a" <> escDQuote <> "b") /\ ("a" <> dquote <> "b")
+        , ("a" <> escSQuote <> "b") /\ ("a" <> squote <> "b")
+        , ("a" <> escBackslash <> "b") /\ ("a" <> backslash <> "b")
+        ]
 
     it "should account for escaped double-quotes within double-quote context" do
-      shouldBeFileArgs "file" [ dquote <> "partA" <> escDQuote <> "partB" <> dquote ]
+      shouldBeFileArgs
+        ("file" /\ "file")
+        [ (dquote <> "a" <> escDQuote <> "b" <> dquote) /\ ("a" <> dquote <> "b")
+        ]
 
     it "should account for escaped single-quotes within single-quote context" do
-      shouldBeFileArgs "file" [ squote <> "partA" <> escSQuote <> "partB" <> squote ]
+      shouldBeFileArgs
+        ("file" /\ "file")
+        [ (squote <> "a" <> escSQuote <> "b" <> squote) /\ ("a" <> squote <> "b")
+        ]
+
+    it "should account for ignore all other escaped chars in a double- or single-quote context" do
+      shouldBeFileArgs
+        ("file" /\ "file")
+        [ (dquote <> "a" <> escSQuote <> "b" <> dquote) /\ ("a" <> escSQuote <> "b")
+        , (squote <> "a" <> escDQuote <> "b" <> squote) /\ ("a" <> escDQuote <> "b")
+        ]
 
