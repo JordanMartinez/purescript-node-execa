81e41353671df860b93d3ee51639e4fca8e98152
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-06-07 21:16:38 -0500

    Run tests via node script

---
+++ ci.mjs
@@ -0,0 +1,10 @@
+import cp from "child_process";
+import process from "process";
+
+const args = process.argv.slice(2);
+if (args[0] === "build") {
+  cp.spawnSync("spago", ["build", "--purs-args", "--censor-lib --strict"], { stdio: "inherit"});
+}
+if (args[0] === "test") {
+  cp.spawnSync("spago", ["-x", "test.dhall", "test"], { stdio: "inherit"});
+}
