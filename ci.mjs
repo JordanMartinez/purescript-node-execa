43b33a37ea2736c88d8fd17e760ec6f8dbb159ab
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-06-07 21:18:48 -0500

    Drop --

--- ci.mjs
+++ ci.mjs
@@ -2,6 +2,8 @@ import cp from "child_process";
 import process from "process";
 
 const args = process.argv.slice(2);
+console.log(`Script CLI args: ${args}`);
+
 if (args[0] === "build") {
   cp.spawnSync("spago", ["build", "--purs-args", "--censor-lib --strict"], { stdio: "inherit"});
 }
